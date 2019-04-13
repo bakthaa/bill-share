4b64e774c74d9f4e7b8ac7540091b41061c7e4cc
baktha <bakthaa@gmail.com>
2019-04-13 07:27:28 -0500

    lazy

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { Link, Route, Switch } from "react-router-dom";
 import { connect } from "react-redux";
 import st from "./index.css";
 import Header from "./component/Header";
-import Manage from "./component/Manage";
-import Home from "./component/Home";
+// import Manage from "./component/Manage";
+// import Home from "./component/Home";
 import BillView from "./component/BillView";
 import Login from "./component/Login";
+
+const Manage = lazy(() => import("./component/Manage"));
+const Home = lazy(() => import("./component/Home"));
 
 class App extends React.Component {
   constructor(props) {
@@ -23,11 +26,13 @@ class App extends React.Component {
         <Header auth={this.props.isAuth} />
         <main>
           {this.props.isAuth ? (
-            <Switch>
-              <Route path="/" exact component={Home} />
-              <Route path="/manage" component={Manage} />
-              <Route path="/bill/:name" component={BillView} />
-            </Switch>
+            <Suspense fallback={<div>Loading...</div>}>
+              <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/manage" component={Manage} />
+                <Route path="/bill/:name" component={BillView} />
+              </Switch>
+            </Suspense>
           ) : (
             <Login />
           )}
